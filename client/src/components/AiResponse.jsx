338172c3cde37c968e9b18d4c11e6c933b171c75
Roshan_chhetri28 <roshanchhetri931@gmail.com>
2025-05-06 12:22:32 +0530

    fix: update API request path in AiResponse component and configure server proxy in vite.config

--- client/src/components/AiResponse.jsx
+++ client/src/components/AiResponse.jsx
@@ -12,7 +12,7 @@ const AiResponse = ({ questionData }) => {
         setLoading(true);
 
         try {
-            const { data } = await axios.post('api/question', {
+            const { data } = await axios.post('/api/question', {
                 question: questionData,
                 query
             });
